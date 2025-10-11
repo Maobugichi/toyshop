f91c4fcb495010b437657c95599ba6db9fcc4477
emekaemmansamuel <emekaemman.samuel@gmail.com>
2025-10-11 17:00:31 +0100

    add review router

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ import mergeRouter from "./router/merge.js";
 import checkoutRouter from "./router/checkoutRouter.js";
 import { checkAuth } from "./check-auth.js";
 import nowRouter from "./router/nowpayment.js";
+import reviewRouter from "./router/review.js";
 
 dotenv.config();
 
@@ -47,6 +48,7 @@ app.use('/api/checkout', checkoutRouter)
 app.use('/api/cart', cartRouter);
 app.use('/api/cart', mergeRouter);
 app.use('/api/payments' , checkAuth , nowRouter);
+app.use('/api/reviews', checkAuth , reviewRouter)
 
 
 server.listen(port,() => {
---
+++ router/review.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import pool from "../db.js";
+
+const reviewRouter = Router();
+
+
+reviewRouter.post("/", async (req, res) => {
+ const user_id = req.user.userId;
+  try {
+    const { username, review, stars } = req.body;
+    if (!user_id || !review || !stars)
+      return res.status(400).json({ error: "Missing required fields" });
+
+    const result = await pool.query(
+      `INSERT INTO reviews (user_id, username, review, stars)
+       VALUES ($1, $2, $3, $4) RETURNING *`,
+      [user_id, username, review, stars]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+
+reviewRouter.get("/", async (req, res) => {
+  try {
+    const result = await pool.query(
+      `SELECT * FROM reviews ORDER BY created_at DESC`
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+export default reviewRouter;
