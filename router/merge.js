64f55419379d8467721d2fa656e1e670d62e8e84
emekaemmansamuel <emekaemman.samuel@gmail.com>
2025-11-30 13:18:38 +0100

    test2

--- router/merge.js
+++ router/merge.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { checkAuth } from "../middleware/check-auth.js";
-import * as mergeController from "../controllers/checkout.controller.js"
+import * as mergeController from "../controllers/mergeControllers.js"
 
 const mergeRouter = Router();
 
