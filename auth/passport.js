694c2765f51dc56d61a45786bfad7ba27849bd4f
emekaemmansamuel <emekaemman.samuel@gmail.com>
2025-11-30 12:33:06 +0100

    fix:correct cartservice import in passport file

--- auth/passport.js
+++ auth/passport.js
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import pool from "../configs/db.js"
-import { getOrCreateCart } from "../cart/cartService.js";
+import { getOrCreateCart } from "../services/cartService.js";
 
 passport.use(new GoogleStrategy(
     {
